7237ec41a8469c0bffc7399dfefd730b8057bd55
lívia <k@kyu.re>
2026-09-15 05:23:52 -0300

    thanks emily²

--- src/components/activity/recent-thoughts.tsx
+++ src/components/activity/recent-thoughts.tsx
@@ -105,6 +105,15 @@ const Styled = css`
 	.thought:first-of-type .thought-details {
 		grid-template-rows: 1fr;
 	}
+	.thought-link:hover .thought-details,
+	.thought-link:focus-visible .thought-details {
+		grid-template-rows: 1fr;
+	}
+
+	:scope:has(.thought:not(:first-of-type) .thought-link:hover) .thought:first-of-type .thought-details,
+	:scope:has(.thought:not(:first-of-type) .thought-link:focus-visible) .thought:first-of-type .thought-details {
+		grid-template-rows: 0fr;
+	}
 
 	.thought-link:hover .thought-details,
 	.thought-link:focus-visible .thought-details {
